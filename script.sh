4a87572bb50b18e6eef88b3f17814c6c3bae026d
ASIF1sMyName2021 <79853297+ASIF1sMyName2021@users.noreply.github.com>
2022-04-06 22:49:38 +0600

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Dwarf Fortress Easy Install Script
+
+# Clear the terminal
+clear
+
+# Welcome Text
+printf "\e[1;32m ┌──────────────────────────┐ \e[0m\n"
+printf "\e[1;32m │  DF Easy Install Script  │ \e[0m\n"
+printf "\e[1;32m └──────────────────────────┘ \e[0m\n"
+printf ""
+
+# Update repository
+apt update && apt upgrade
+clear
+
+# Install Wget
+printf "Installing wget...\n\n"
+apt install wget
+clear
+
+# Install box64
+apt install gnupg
+wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
+wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | apt-key add -
+
+\cp files/box64.list /etc/apt/sources.list.d/
+
+apt update
+clear
+printf "Installing box64...\n\n"
+apt install box64
+clear
+
+# Download Dwarf Fortress
+printf "Downloading DWARF FORTRESS...\n\n"
+wget http://www.bay12games.com/dwarves/df_47_05_linux.tar.bz2
+
+# Extract Archive
+tar -xf df_47_05_linux.tar.bz2
+rm -r df_47_05_linux.tar.bz2
+mv df_linux DF
+mv DF ~
+
+# Append box64 in df file
+\cp files/df ~/DF/
+
+# Change DF settings
+\cp files/init.txt ~/DF/data/init/
+
+# Install DF dependencies
+apt install libsdl1.2debian libgtk2.0-0 libsdl-image1.2 libglu1-mesa libsdl-ttf2.0-0 libopenal-dev libsndfile1-dev libncursesw5
+
+# Clear the terminal
+clear
+
+# Completion Message
+printf "\e[1;31m ◆◆ Dwarf Fortress Installed ◆◆ \e[0m\n"
+printf "\e[1;31m ------------------------------ \e[0m\n"
+printf "\e[1;32m    Start DF to check it out! \e[0m\n"
+printf ""
+
